d7151c38e1aeb6e6836ed17259491faf05e27da2
Galván Lautaro <105556896+GalvanLautaro@users.noreply.github.com>
2024-05-14 23:40:44 -0300

    Add files via upload

---
+++ 07ClasesAbstractasyHerencia2/src/src/Arquero.java
@@ -0,0 +1,23 @@
+package src;
+
+public class Arquero extends Jugador{
+	 private double efectividad;
+
+	    public Arquero(String nombre, double velocidad, double potencia, double efectividad) {
+	        super(nombre, velocidad, potencia);
+	        if (efectividad < 0 || efectividad > 1) {
+	            throw new IllegalArgumentException("La efectividad del arquero debe estar entre 0 y 1");
+	        }
+	        this.efectividad = efectividad;
+	    }
+
+	    @Override
+	    public double indiceDefensa() {
+	        return efectividad;
+	    }
+
+	    @Override
+	    public double indiceAtaque() {
+	        return 0.1 * getVelocidad() * getPotencia();
+	    }
+}
---
+++ 07ClasesAbstractasyHerencia2/src/src/Equipo.java
@@ -0,0 +1,33 @@
+package src;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Equipo {
+	private List<Jugador> jugadores;
+
+    public Equipo(String nombre) {
+        this.jugadores = new ArrayList<>();
+    }
+
+    public void agregarJugador(Jugador jugador) {
+        jugadores.add(jugador);
+    }
+
+    public void eliminarJugador(Jugador jugador) {
+        jugadores.remove(jugador);
+    }
+
+    public List<Jugador> getJugadores() {
+        return jugadores;
+    }
+
+    public double indiceDefensa() {
+        return jugadores.stream().mapToDouble(Jugador::indiceDefensa).sum();
+    }
+
+    public double indiceAtaque() {
+        return jugadores.stream().mapToDouble(Jugador::indiceAtaque).sum();
+    }
+
+}
